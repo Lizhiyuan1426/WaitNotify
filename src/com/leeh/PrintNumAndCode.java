8a8913bb3ae72684010200aa11bc610ec6fde1d8
李恒 <349788131@qq.com>
2018-03-16 23:04:34 +0800

    判断放在唤醒下面, 避免两个线程死锁

--- src/com/leeh/PrintNumAndCode.java
+++ src/com/leeh/PrintNumAndCode.java
@@ -7,6 +7,7 @@ import java.util.concurrent.locks.ReentrantLock;
 class NumAndCode{
 	private char code = 'A' - 1;
 	private int num = 0;
+	
 	Lock lock = new ReentrantLock();
 	Condition condition = lock.newCondition();
 	
@@ -14,11 +15,15 @@ class NumAndCode{
 		lock.lock();
 		try {
 			// 判断
-			while(num != 52) {
+			while(num < 52) {
+				
 				// 干活
 				System.out.print(++num+""+(++num));
 				// 通知
 				condition.signalAll();
+				if(num == 52) {
+					return;
+				}
 				condition.await();
 			}
 		} catch (Exception e) {
@@ -32,12 +37,15 @@ class NumAndCode{
 		lock.lock();
 		try {
 			// 判断
-			while(code != 'Z') {
+			while(code < 'Z') {
 				// 干活
 				code=(char)(code + 1);
 				System.out.print(code);
 				// 通知
 				condition.signalAll();
+				if(code == 'Z') {
+					return;
+				}
 				condition.await();
 			}
 		} catch (Exception e) {
@@ -54,16 +62,11 @@ public class PrintNumAndCode {
 		NumAndCode nc = new NumAndCode();
 		
 		new Thread(() -> {
-//			for (int i = 0; i < 10; i++) {
-				nc.printNum();
-//			}
+			nc.printNum();
 		}, "Num:\t").start();
 		
 		new Thread(() -> {
-//			for (char i = 'A'; i < 'Z'; i++) {
-				
-				nc.printCode();;
-//			}
+			nc.printCode();;
 		}, "Code:\t").start();
 	}
 
